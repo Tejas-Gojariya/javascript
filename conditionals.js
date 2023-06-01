1539fba1293a093d9b1718456730945443e38c79
tejas_Gojariya <109208189+Tejas-Gojariya@users.noreply.github.com>
2023-06-01 22:48:18 +0530

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,49 @@
+// if-else statement
+
+let age = 25;
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+// else-if
+
+console.log(testnum(15));
+
+function testnum(a) {
+    let result;
+    if (a > 0) {
+        result = 'positive';
+    } else {
+        result = 'not positive';
+    }
+    return result;
+}
+
+// nested-if-else
+
+let num = 7;
+if (num < 10) {
+    if (num < 5) {
+        console.log("Number is less then 5");
+    } else {
+        console.log("Number is greater then 5 but less then 10");
+    }
+}
+
+// nested-if-else
+
+var temperature = 90;
+var time = "morning";
+
+if (temperature > 30) {
+    if (time === "morning") {
+        console.log("It's hot in the morning!");
+    } else {
+        console.log("It's hot in the afternoon or evening!");
+    }
+} else {
+    console.log("It's not that hot today.");
+}
