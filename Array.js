08a2c4b10b06faf0d9143307bf552f6743a6c752
tejas_Gojariya <109208189+Tejas-Gojariya@users.noreply.github.com>
2023-06-06 23:55:32 +0530

    Add files via upload

--- Array.js
+++ Array.js
@@ -12,13 +12,11 @@ let fruitList = ["apple", "banana", "kiwi", ];
 
 
 // push - add a new items to the end of an array 
-
 console.log(fruitList.push("watermelon"));
 console.log(fruitList);
 
 
 // pop - removes the last element of an array
-
 console.log(fruitList.pop());
 console.log(fruitList);
 
@@ -29,7 +27,6 @@ console.log(fruitList);
 
 
 // unswift - adds new elements to the beginning of an array. 
-
 console.log(fruitList.unshift("mango"));
 console.log(fruitList);
 console.log(fruitList.unshift("greps"));
@@ -43,7 +40,6 @@ console.log(nums.includes(5, 2)); //(key,startIndex)
 
 
 // indexOf() - return first index if found else -1 (use ===)
-
 arr1 = ["IND", "USA", "UK", "IND"];
 console.log(arr1.indexOf("UK"));
 console.log(arr1.indexOf("CN"));
@@ -51,7 +47,25 @@ console.log(arr1.indexOf("IND")); // only return 0 (first index)
 console.log(arr1.indexOf("IND", 2)); // start searching from index 2
 
 // join - not change og array , convert array into string
-
 fruitList = ["apple", "banana", "kiwi", "greps"];
 console.log(fruitList.join("_")); // in "" give ant delimiter
 console.log(fruitList);
+
+
+// concat() - merge to or more array not modify og array , return shallow copy
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+console.log(array3);
+
+
+// flat
+let arr = ["a","b", "c", ["d", "e", "f", ["g", "h", "i", ["tejas", ["abc", ["def"]]]]],
+  ];
+  
+  console.log(arr.flat()); // return new array
+  console.log(arr.flat(2)); // 2 level
+  console.log(arr.flat(3));
+  console.log(arr.flat(Infinity));
+  
