221a9fb8e840a88c0ca405e10ac3e32e78465bdd
tejas_Gojariya <109208189+Tejas-Gojariya@users.noreply.github.com>
2023-06-06 19:45:29 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,14 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
