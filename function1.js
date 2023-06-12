4ab1788e42743b840052e05e531cfd62f6ee02fc
tejas_Gojariya <109208189+Tejas-Gojariya@users.noreply.github.com>
2023-06-12 22:10:55 +0530

    function in js

---
+++ function1.js
@@ -0,0 +1,16 @@
+function calculatecartprice(...num1){
+    return num1
+}
+
+// console.log(calculatecartprice(200, 400 , 500))
+
+const user={
+    username :"tejas",
+    price : 1000
+}
+ 
+function handleobject(anyobject){
+    console.log(`usernam is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleobject(user)
