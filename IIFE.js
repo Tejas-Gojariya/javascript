71ebfca26231de1c6d444946e4866e81fef5f38b
tejas_Gojariya <109208189+Tejas-Gojariya@users.noreply.github.com>
2023-06-16 16:44:39 +0530

    IIFE in js

---
+++ IIFE.js
@@ -0,0 +1,10 @@
+//  immeditely invoked function expression (IIFE)
+/* The main purpose of an IIFE is to create a private scope for your code, 
+preventing variables and functions from polluting the global namespace.*/
+
+
+(function one (){
+    console.log(`DB CONNECTED`)
+})()
+
+
